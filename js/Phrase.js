d9928794e36a079efbec222d60bfa5dec816958a
Micah Hobbs <micahthobbs@gmail.com>
2019-04-04 21:34:59 +1100

    checkLetter() fun

--- js/Phrase.js
+++ js/Phrase.js
@@ -21,13 +21,29 @@ class Phrase {
       if (listItem.textContent === ` `) {
         listItem.classList.add(`space`);
       } else {
-        listItem.classList.add(`letter`, `hide`);
+        listItem.classList.add(`hide`, `letter`, `${phraseAsArray[i]}`);
       }
       phraseList.appendChild(listItem);
     }
   }
 
-  checkLetter(letter) {}
+  checkLetter(guess) {
+    const letters = document.getElementsByClassName(`letter`);
+    const userChoice = guess;
+    const match = false;
+    for (let i = 0; i < letters.length; i += 1) {
+      if (letters[i].textContent === userChoice) {
+        // showMatchedLetter(letters[i])
+        match = true;
+      }
+    }
+    return match;
+  }
 
-  showMatchedLetter(letter) {}
+  showMatchedLetter(element) {
+    element.classList.remove(`hide`);
+    element.classList.add(`show`);
+  }
+  // loop through phase array
+  // check each letter and if matching add show class
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const keys = document.getElementById(`qwerty`);
+
+keys.for;
